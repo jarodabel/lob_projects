1080b053fb4901b2e7b8b9bd1db0c76fb3781b4a
arodjabel <arodjabel@gmail.com>
2016-10-18 21:13:44 -0500

    some new stuff

--- lob_script.js
+++ lob_script.js
@@ -1,57 +1,70 @@
 fs = require('fs');
 
-var filePath = './rmhw/lob_patient_statement.html';
+var filePath = './templates/generic_no_table.html';
+
+var serviceHtml = "" +
+    "<div class='service-list-row'>" +
+    "<div class='service-list-cell'>service one</div>" +
+    "<div class='service-list-cell'>$80.00</div>" +
+    "</div>" +
+    "<div class='service-list-row'>" +
+    "<div class='service-list-cell'>service two</div>" +
+    "<div class='service-list-cell'>$40.00</div>" +
+    "</div>";
+
+// serviceHtml = JSON.stringify(serviceHtml.html)
+//     .replace(new RegExp('\/', 'g'), '&#92;')
+//     .replace(new RegExp('"', 'g'), '');
 
 
+function getDataObj(key, fileData) {
+    var date = new Date();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var year = date.getFullYear();
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
 
-fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
-    if (!err) {
-        var date = new Date();
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        var year = date.getFullYear();
-        var monthNames = ["January", "February", "March", "April", "May", "June",
-            "July", "August", "September", "October", "November", "December"
-        ];
+    var today = monthNames[month] + ' ' + day + ', ' + year;
 
-        var today = monthNames[month] + ' ' + day + ', ' + year;
-
-        var Lob = require('Lob')('test_546c65dfaf8aa7ce4514678e4a0bb7fae74', {
-            apiVersion: '2016-06-30'
-        });
-
-        var temp_obj = {
-            description: "Template Matias",
+    if (key === 'temp_obj_no_table') {
+        fileData = fileData.replace('serviceListReplaceBlock', serviceHtml);
+    }
+    var dataStore = {
+        temp_obj_simple: {
+            description: "Generic Simple Example",
             to: {
-                name: 'Nelson Cat',
+                name: 'Nelson SM',
                 address_line1: '123 Test Street',
                 address_line2: 'Suite 12',
-                address_city: 'Mountain View',
+                address_city: '',
                 address_state: 'CA',
                 address_zip: '94041',
-                address_country: 'US',
+                address_country: 'US'
             },
             from: {
-                name: 'Your Health Clinc',
+                name: 'Southwest Catus Clinic',
                 address_line1: '123 Test Avenue',
-                address_city: 'Mountain View',
-                address_state: 'CA',
-                address_zip: '94041',
-                address_country: 'US',
+                address_city: 'Kansas City',
+                address_state: 'MO',
+                address_zip: '64110',
+                address_country: 'US'
             },
-            file: data,
+            file: fileData,
             color: false,
             double_sided: false,
             data: {
-                name: 'Nelson Cat',
+                name: 'Nelson Smith',
                 balance: '$75.00',
                 claimId: '123654',
                 accountNumber: '3728947',
                 todaysDate: today,
                 visitDate: '3/26/2016',
+                clinicname: 'Southwest Catus Clinic',
                 providerName: "Patches O'Houlihan",
                 facilityNumber: '(816) 555-5655',
-                facilityName: 'Dog Spa',
+                facilityName: 'Hillside',
                 address_city: 'Mountain View',
                 address_state: 'CA',
                 today: today,
@@ -60,9 +73,52 @@ fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
                 email: 'info@hemmahealth.com',
                 website: 'hemmahealth.com'
             }
-        }
-
-        var rmhw_obj = {
+        },
+        temp_obj_no_table: {
+            description: "Generic No Table Example",
+            to: {
+                name: 'Nelson Smith',
+                address_line1: '123 Test Street',
+                address_line2: 'Apt 12',
+                address_city: "Lee's Summit",
+                address_state: 'MO',
+                address_zip: '64063',
+                address_country: 'US'
+            },
+            from: {
+                name: 'Southmoreland Clinic',
+                address_line1: '5149 Oak Avenue',
+                address_city: 'Kansas City',
+                address_state: 'MO',
+                address_zip: '64110',
+                address_country: 'US'
+            },
+            file: fileData,
+            color: false,
+            double_sided: false,
+            data: {
+                name: 'Nelson Smith',
+                balance: '$120.00',
+                claimId: '123654',
+                accountNumber: '3728947',
+                todaysDate: today,
+                visitDate: '3/26/2016',
+                clinicName: 'Southmoreland Clinic',
+                providerName: "Patches O'Houlihan",
+                facilityNumber: '(816) 555-5655',
+                facilityName: 'Gillham Park',
+                address_line_1: '5149 Oak Avenue',
+                address_city: 'Kansas City',
+                address_state: 'MO',
+                address_zip: '64110',
+                today: today,
+                subtotal: '$70.00',
+                taxfees: '$5.00',
+                email: 'info@hemmahealth.com',
+                website: 'hemmahealth.com'
+            }
+        },
+        rmhw_obj: {
             description: "Rocky Mountain Health Wellness Example",
             to: {
                 name: 'John Doe',
@@ -82,7 +138,7 @@ fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
                 address_zip: '83616',
                 address_country: 'US',
             },
-            file: data,
+            file: fileData,
             color: false,
             double_sided: false,
             data: {
@@ -104,13 +160,27 @@ fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
                 website: '<span>hello</span>'
             }
         }
+    };
+    return dataStore[key]
+}
 
-        Lob.letters.create(rmhw_obj, function (err, res) {
+function readFunction(err, data) {
+    if (!err) {
+
+        var dataObj = getDataObj('temp_obj_no_table', data);
+
+        var Lob = require('Lob')('test_546c65dfaf8aa7ce4514678e4a0bb7fae74', {
+            apiVersion: '2016-06-30'
+        });
+
+        Lob.letters.create(dataObj, function (err, res) {
             console.log(err, res);
         });
     } else {
         console.log(err);
     }
-});
+}
+
+fs.readFile(filePath, {encoding: 'utf-8'}, readFunction);
 
 
